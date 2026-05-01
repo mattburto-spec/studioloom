8b0100e79d91740a3a1e884e7ca4ac0de3fffa56
Matt Burton <mattburto@gmail.com>
2026-05-01 22:24:48 +0800

    claim(migrations): reserve phase_3_4f is_teacher_of_student class_members + mentors

---
+++ supabase/migrations/20260501142442_phase_3_4f_is_teacher_of_student_includes_class_members_and_mentors.down.sql
@@ -0,0 +1,4 @@
+-- Rollback for: phase_3_4f_is_teacher_of_student_includes_class_members_and_mentors
+-- Pairs with: 20260501142442_phase_3_4f_is_teacher_of_student_includes_class_members_and_mentors.sql
+
+-- TODO: write rollback SQL here
