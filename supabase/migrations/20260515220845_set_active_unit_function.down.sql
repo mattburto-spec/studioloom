cde5b1e70be87b3723d5ae8b09c8cdadd565676c
Matt Burton <mattburto@gmail.com>
2026-05-16 06:09:59 +0800

    set_active_unit SECURITY DEFINER helper with auth gate (migration 20260515220845)

--- supabase/migrations/20260515220845_set_active_unit_function.down.sql
+++ supabase/migrations/20260515220845_set_active_unit_function.down.sql
@@ -1,4 +1,4 @@
 -- Rollback for: set_active_unit_function
 -- Pairs with: 20260515220845_set_active_unit_function.sql
 
--- TODO: write rollback SQL here
+DROP FUNCTION IF EXISTS public.set_active_unit(uuid, uuid);
